2b9f16f53febbe51e216276efc605af89e35e269
spaluszkiewicz04 <stanislaw.paluszkiewicz@student.put.poznan.pl>
2026-05-07 10:53:14 +0200

    Menu główne

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace lab8
 {
@@ -7,6 +8,25 @@ namespace lab8
         public MainWindow()
         {
             InitializeComponent();
+
+            this.FindControl<Button>("BtnAddPlayer").Click += OpenAddPlayer;
+            this.FindControl<Button>("BtnHistory").Click += OpenHistory;
+
+            this.FindControl<Button>("BtnGame1").Click += (s, e) => { /* Gra 1 */ };
+            this.FindControl<Button>("BtnGame2").Click += (s, e) => { /* Gra 2 */ };
+            this.FindControl<Button>("BtnGame3").Click += (s, e) => { /* Gra 3 */ };
+        }
+
+        private void OpenAddPlayer(object sender, RoutedEventArgs e)
+        {
+            //var playerWindow = new PlayerWindow();
+            //playerWindow.ShowDialog(this);
+        }
+
+        private void OpenHistory(object sender, RoutedEventArgs e)
+        {
+            //var historyWindow = new HistoryWindow();
+            //historyWindow.Show();
         }
     }
 }
---
+++ models.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace lab8
+{
+    public class Player
+    {
+        public string Login { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class MatchRecord
+    {
+        public string GameName { get; set; }
+        public string WinnerLogin { get; set; }
+        public string Date { get; set; }
+    }
+
+    public static class AppState
+    {
+        public static List<Player> Players { get; set; } = new List<Player>();
+        public static List<MatchRecord> MatchHistory { get; set; } = new List<MatchRecord>();
+    }
+}
